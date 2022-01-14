07d79cbf624597669f5bb552849091304a53bf21
tenzin1308 <56772873+tenzin1308@users.noreply.github.com>
2022-01-14 18:11:13 -0500

    build: initialized the app

--- App.js
+++ App.js
@@ -1,15 +1,7 @@
-import { View, Text } from "react-native";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
+    <Home />
   );
 }
---
+++ pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+import Nav from '../components/home/Nav'
+
+const Home = () => {
+    return (
+        <SafeAreaView>
+            <Nav />
+        </SafeAreaView>
+    )
+}
+
+export default Home
