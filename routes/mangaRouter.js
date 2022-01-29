7e9d3775695b5180575b16c075e7d236f6b372b9
tenzin1308 <56772873+tenzin1308@users.noreply.github.com>
2022-01-29 12:58:54 -0500

    fix: changes api

--- routes/mangaRouter.js
+++ routes/mangaRouter.js
@@ -3,10 +3,18 @@ import readmModel from "../models/readmModel.js";
 
 const mangaReader = express.Router();
 
-mangaReader.route("/readm/").get(async (req, res) => {
+mangaReader.route("/readm/titles").get(async (req, res) => {
     const mangas = await readmModel.find();
     if (mangas.length > 0) {
-        return res.json(mangas);
+        return res.json(mangas.map(manga => manga.manga_name));
+    }
+    return res.json({ message: "No manga readers found" });
+});
+
+mangaReader.route("/readm/titles/manga_chapters").get(async (req, res) => {
+    const manga = await readmModel.findOne({"manga_name": req.query.manga_name});
+    if (manga.length > 0) {
+        return res.json(manga.manga_chapters);
     }
     return res.json({ message: "No manga readers found" });
 });
